e71fc603bc5b92a263d3d0e3f82b7339f757630a
seesmof <110289150+seesmof@users.noreply.github.com>
2022-10-23 10:20:39 +0300

    build: :rocket: init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Salary_Calculator
- 
+<h2 align="center">BMI Calculator</h2>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![GitHub Issues](https://img.shields.io/github/issues/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/Salary_Calculator/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/seesmof/The-Documentation-Compendium.svg)](https://github.com/seesmof/Salary_Calculator/pulls)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](./LICENSE)
+
+</div>
+
+---
+
+## 📝 Table of Contents
+
+- [📝 Table of Contents](#-table-of-contents)
+- [🧐 About <a name = "about"></a>](#-about-)
+- [💿 Working Principle <a name = "built_using"></a>](#-working-principle-)
+- [⛏️ Built Using <a name = "built_using"></a>](#️-built-using-)
+- [✍️ Authors <a name = "authors"></a>](#️-authors-)
+
+## 🧐 About <a name = "about"></a>
+
+Basic salary calculator made with C++ only to get a bit of practice on dynamic arrays and arrays in general.
+
+## 💿 Working Principle <a name = "built_using"></a>
+
+- Program asks the user to enter the amount of months they have been saving for
+- Program asks to enter the amount of money they saved each months via a for loop
+- Program calculates the total amount of money that has been saved, the average amount and predicts the amount they will have in some time, depending on how long they already have been saving for
+
+## ⛏️ Built Using <a name = "built_using"></a>
+
+- [C++](https://cplusplus.com/) - Programming language
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@seesmof](https://github.com/seesmof) - Development
---
+++ index.cpp
@@ -0,0 +1,60 @@
+// include necessary libraries
+#include <iostream>
+using namespace std;
+
+// start main function
+int main(int argc, char **argv)
+{
+    // output progrma intro
+    cout << endl;
+    cout << "***************************************** Salary Calculator ********************************************" << endl
+         << endl;
+
+    int monthsCount;
+    cout << "Please enter the amount of months you have been saving for: ";
+    cin >> monthsCount;
+    // create an array with sixth months worth of savings, create a for loop for asking for the amount saved each month
+    double *monthsArray = new double[monthsCount];
+    double total = 0.0;
+    int i;
+    for (i = 0; i < monthsCount; i++)
+    {
+        cout << "Enter the amount of savings you did the " << i + 1 << " month $";
+        cin >> monthsArray[i];
+
+        total += monthsArray[i];
+    }
+
+    double average = total / i;
+    cout << endl;
+    cout << "Your total savings are $" << total << endl;
+    cout << "You have been saving an average of $" << average << " a month" << endl;
+    if (monthsCount < 6)
+    {
+        double tilYearEnd = average * 6;
+        cout << "If you will keep this rate of savings up, you will get $" << tilYearEnd << " till the end of this year" << endl;
+    }
+    else if (monthsCount > 6 && monthsCount < 12)
+    {
+        double inYear = average * 12;
+        cout << "If you will keep this rate of savings up, you will get $" << inYear << " in a year" << endl;
+    }
+    else if (monthsCount > 12 && monthsCount < 24)
+    {
+        double inTwoYears = average * 24;
+        cout << "If you will keep this rate of savings up, you will get $" << inTwoYears << " in two years" << endl;
+    }
+    else
+    {
+        double inFiveYears = average * 60;
+        cout << "If you will keep this rate of savings up, you will get $" << inFiveYears << " in five years" << endl;
+    }
+
+    // output program outro
+    cout << endl;
+    cout << "********************************************************************************************************" << endl;
+
+    // end main function
+    system("pause");
+    return 0;
+}
